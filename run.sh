46836a640abc523b52ff7b98f1cb1334f7bac0bd
Timo <timoguelich@gmail.com>
2024-10-28 23:51:34 +0100

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# create venv
+# deactivate old venv if active
+if [ -d "venv" ]; then
+    # Check if the virtual environment is active
+    if [[ "$VIRTUAL_ENV" != "" ]]; then
+        deactivate
+    fi
+    
+    # delete old venv
+    echo "Deleting old virtual environment..."
+    rm -rf venv
+fi
+
+# create new venv
+echo "Creating new virtual environment..."
 python -m venv venv
 
-# activate venv
+# activate new venv
+echo "Activating virtual environment..."
 source venv/bin/activate
 
 # install requirements
+echo "Installing requirements..."
+pip install --upgrade pip  # Upgrade pip to the latest version
 pip install -r requirements.txt
 
 # Run the app with Uvicorn
+echo "Running the app with Uvicorn..."
 uvicorn main:app --host 0.0.0.0 --port 8000
