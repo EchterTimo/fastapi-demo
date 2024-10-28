c9fa46e1d9fa1ef56d28078bd9bd9d00b71644cd
Timo <57227498+EchterTimo@users.noreply.github.com>
2024-10-29 00:15:24 +0100

    added ip endpoint

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "it works"}
+
+@app.get("/ip")
+async def get_client_ip(request: Request):
+    client_ip = request.client.host
+    return {"ip": client_ip}
